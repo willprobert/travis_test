7dd41d7671a3fe0ffa5bf2e8027c1a163f2a9ded
willprobert <62729041+willprobert@users.noreply.github.com>
2020-03-27 00:40:50 +0000

    First commit

---
+++ tests/test_check.py
@@ -0,0 +1,10 @@
+import numpy as np, pandas as pd
+
+def test_summation():
+    np.testing.assert_equal(0, 0)
+
+def test_summation2():
+    np.testing.assert_equal(2+2, 4)
+
+def test_summation3():
+    np.testing.assert_equal(2+3, 5)
